0eada19e1a310d2d8c05a4b6802ba85ef8a6e78f
Gaurav Kumar <gauravk26800@gmail.com>
2021-05-16 23:55:06 +0530

    completed join component added css file to both components

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Join.css";
 
 function Join(props) {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
   return (
-    <section className="join">
-      <h1>Join</h1>
-    </section>
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            placeholder="Name"
+            className="joinInput"
+            type="text"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+
+        <div>
+          <input
+            placeholder="Room"
+            className="joinInput mt-20"
+            type="text"
+            onChange={(e) => {
+              setRoom(e.target.value);
+            }}
+          />
+        </div>
+
+        <Link
+          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="button mt-20" type="submit">
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
   );
 }
 

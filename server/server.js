17609f1d3936d593a50f195baaadb618c2021366
Gaurav Kumar <gauravk26800@gmail.com>
2021-05-17 01:11:11 +0530

    added users helper js added socket.emit on admin connect

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 
 const router = require("./router");
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+const { get } = require("./router");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -15,13 +17,36 @@ app.use(cors());
 app.use(router);
 
 io.on("connect", (socket) => {
-  console.log("We have a new connection!!!");
+  // console.log("We have a new connection!!!");
 
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.name)
+      .emit("message", { user: "admin", text: `${user.name}, has joined` });
+
+    socket.join(user.room);
+    callback();
+
+    // console.log(name, room);
 
     // const error = true;
     // if (error) callback({ error: "error" });
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
   });
 
   socket.on("disconnect", () => {

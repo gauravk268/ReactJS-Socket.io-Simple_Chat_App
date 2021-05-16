7df971ecc2be98c4cdba50d714418bccf079e3ba
Gaurav Kumar <gauravk26800@gmail.com>
2021-05-17 00:52:03 +0530

    fixed cors in server added css from tutorial repo server is working correctly

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 import "./Chat.css";
 
-function Chat(props) {
+let socket;
+
+function Chat({ location }) {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "localhost:5000/";
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    const connectionOptions = {
+      // "force new connection": true,
+      reconnectionAttempts: "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
+      timeout: 10000, //before connect_error and connect_timeout are emitted.
+      transports: ["websocket"],
+    };
+    socket = io(ENDPOINT, connectionOptions);
+
+    setName(name);
+    setRoom(room);
+    socket.emit("join", { name, room }, () => {});
+
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+
   return (
     <section className="chat">
       <h1>Chat</h1>
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const cors = require("cors");
 
 const router = require("./router");
 
@@ -10,14 +11,22 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on("connextion", (socket) => {
+app.use(cors());
+app.use(router);
+
+io.on("connect", (socket) => {
   console.log("We have a new connection!!!");
+
+  socket.on("join", ({ name, room }, callback) => {
+    console.log(name, room);
+
+    // const error = true;
+    // if (error) callback({ error: "error" });
+  });
 
   socket.on("disconnect", () => {
     console.log("User had left!!!");
   });
 });
-
-app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port: ${PORT}`));

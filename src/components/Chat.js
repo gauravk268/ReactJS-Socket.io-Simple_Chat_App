608820d057ae5aae5057089b7d7374a93c965bd3
Gaurav Kumar <gauravk26800@gmail.com>
2021-06-25 17:09:16 +0530

    FIX: updated backend server link

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -15,7 +15,7 @@ function Chat({ location }) {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [users, setUsers] = useState([]);
-  const ENDPOINT = "localhost:5000/";
+  const ENDPOINT = "https://gauravk268-simple-chat-app.herokuapp.com/";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
